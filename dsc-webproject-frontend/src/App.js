1e5cf183ad3513ca50735b638076e2d4813aad41
mori8 <kaithape@gmail.com>
2020-11-11 20:49:33 +0900

    Create customAxios.js

--- dsc-webproject-frontend/src/App.js
+++ dsc-webproject-frontend/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import customAxios from './customAxios';
 
 function App() {
+  // IP주소 변수 선언
+  const [ip, setIp] = useState('');
+
+  // IP주소 값을 설정합니다.
+  function callback(data) {
+    setIp(data);
+  }
+
+  // 첫번째 렌더링을 다 마친 후 실행합니다.
+  useEffect(
+    () => {
+      // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
+      customAxios('/ip', callback);
+    }, []
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        이 기기의 IP주소는 {ip}입니다.
       </header>
     </div>
   );
